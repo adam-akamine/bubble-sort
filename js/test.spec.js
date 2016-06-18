f0373a61fb2ebd5451e2cecc5b897b611f50a9fd
kekuso <kekuso808@gmail.com>
2016-06-18 09:50:01 -1000

    Created test. Imported mocha.

--- js/test.spec.js
+++ js/test.spec.js
@@ -1,8 +1,9 @@
 var expect = chai.expect;
 
-describe('bubbleSort()'), function () {
+describe('bubbleSort()', function () {
 
   it('should sort values from lowest to highest', function () {
     expect(bubbleSort([2, 1])).to.equal([1,2]);
   });
-}
+
+});
