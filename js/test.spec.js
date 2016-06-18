9c5b7ccd2b1920f81ee71ab39ed944475e1cc97e
kekuso <kekuso808@gmail.com>
2016-06-18 09:44:04 -1000

    first commit.

---
+++ js/test.spec.js
@@ -0,0 +1,8 @@
+var expect = chai.expect;
+
+describe('bubbleSort()'), function () {
+
+  it('should sort values from lowest to highest', function () {
+    expect(bubbleSort([2, 1])).to.equal([1,2]);
+  });
+}
